32789beb00933664fa4e4c785a2acbf9e892e8a1
Sylvain <sylvain.guo@grenoble-inp.org>
2022-03-11 10:33:51 +0100

    new_usine

--- usine.py
+++ usine.py
@@ -1,6 +1,6 @@
 
 from magasin import Magasin 
-
+from typing import List
 class Usine :
     def __init__(self,Capa_stocku: int ,Cout_stocku : float ,Stock_initu : int, Stock_finalu : int 
     , Capa_produ : float, Cout_produ : float ) -> None: 
@@ -11,24 +11,43 @@ class Usine :
         self.stock_fin : int = Stock_finalu
         self.cap_prod : float = Capa_produ
         self.cout_prod : float = Cout_produ
+        self.Lcommande: List[Magasin] = []
 
     #def capacité_prod(self,magasin: Magasin):
-        
+    
 
     def production(self, magasin: Magasin) -> int :
         '''renvoie la charge du Camion, i.e. la quantite des commandes presentes 
         dans le camion '''
-        contenue : int = 0
-        for i in range (len(magasin.commande)):
-            contenue = self.commande_camion[i].quantite + contenue
-        return contenue
+        if self.cap_prod<magasin.commande():
+            return self.cap_prod
+        return magasin.commande()
 
-    def capacite_restante(self) -> int:
+    def cap_prod_restante(self) -> int:
         '''renvoie la capacite restante de ce Camion (en tenant compte de
         tout ce qui a deja ete ajoute)'''
         return self.cap_prod - self.production()
 
+    def demande_totale(self) -> int:
+        '''renvoie la charge du Camion, i.e. la quantite des commandes presentes 
+        dans le camion '''
+        qt: int = 0
+        for k in range (len(self.Lcommande)):
+            if self.Lcommande[k] != None: ### is not None:
+                qt = qt + self.Lcommande[k].commande()
+        return qt
     
+    def capacite_restante(self) -> int:
+        '''renvoie la capacite restante de ce Camion (en tenant compte de
+        tout ce qui a deja ete ajoute)'''
+        return self.cap_prod - self.demande_totale()
+        
+    
+    def peut_contenir(self, com: Magasin) -> bool:
+        ''' renvoie vrai si et seulement si la commande c peut etre ajoutee à
+        ce Camion sans de#TODOer la capacite (en tenant compte de tout
+        ce qui a deja été ajoute)'''
+        return self.capacite_restante()>=com.commande
 
 
 
