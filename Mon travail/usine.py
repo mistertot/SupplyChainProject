ca55de406240097b3f3899ff48bb3820e683f8c4
Run41 <romain.hoarau@grenoble-inp.org>
2022-03-11 09:12:03 +0100

    Solution_trivial

---
+++ Mon travail/usine.py
@@ -0,0 +1,27 @@
+
+import Magasin from Magasin
+
+class Usine :
+    def __init__(self,Capa_stocku: int ,Cout_stocku : float ,Stock_initu : int, Stock_finalu : int 
+    , Capa_produ : float, Cout_produ : float ) -> None: 
+    
+        self.cap_stock : int = Capa_stocku
+        self.cout_stock : float = Cout_stocku
+        self.stock_int : int = Stock_initu
+        self.stock_fin : int = Stock_finalu
+        self.cap_prod : float = Capa_produ
+        self.cout_prod : float = Cout_produ
+
+    def produire (self,):
+
+    
+
+
+
+
+
+
+
+
+
+
