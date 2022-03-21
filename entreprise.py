b5e74924339485fbe3f82cfe17b910167d9a4ea6
Sylvain <sylvain.guo@grenoble-inp.org>
2022-03-21 16:31:16 +0100

    entrepriseV2

--- entreprise.py
+++ entreprise.py
@@ -2,17 +2,23 @@ from magasin import Magasin
 from typing import List
 from usine import Usine
 from entrepot import Entrepot
-
+from transport import Transport
+from traitement_de_texte import recup_sites
 
 class Entreprise :
 
-    def __init__(self,nbMagasin,nbUsine,nbEntrepot):
-        self.Lcommande: List[Magasin] = []
-        self.Lusine: List [Usine] = []
+    def __init__(self, path : str):
+        self.usines: List [Usine] = recup_sites(path)[0]
+        self.entrepots: List[Entrepot] = recup_sites(path)[1]
+        self.magasins: List[Magasin] = recup_sites(path)[2]
         
+
+    def __repr__(self) -> str:
+        return "( entreprise: {0}; {1}; {2} ".format(self.usines,self.entrepots,self.magasins) 
+    
     def nb_de_commande_total(self):
         com_tot = 0
-        for k in range (len(self.Lcommande)):
+        for k in range (len(self.magasins)):
             com_tot = com_tot + self.Lcommande[k].commande 
         return com_tot
 
@@ -29,10 +35,15 @@ class Entreprise :
         return qte_prod_restante
 
     def cout_usine(u:Usine):
-        return (u.cout_stock + u.cout_prod)
+        return (u.cout_stock + u.cout_prod )
 
-    def cout_total(self) -> float:
-        for m in self.Lcommande:
+    #def entrepot_magasin(self,m:Magasin):
+     #   for e in self.Lentrepot:
+
+a = Entreprise("inst\B7a-sites.txt")
+print(a)
+
+
 
     
 
--- magasin.py
+++ magasin.py
@@ -17,7 +17,7 @@ class Magasin :
         #self.hist : str =  Hist_demandee 
 
     def __repr__(self) -> str:
-        return "Entrepot : {0}; {1}; {2}; {3}".format(self.cap_stock, self.cout_stock, self.stock_int, self.stock_fin)
+        return "Magasin : {0}; {1}; {2}; {3}".format(self.cap_stock, self.cout_stock, self.stock_int, self.stock_fin)
 
 
 
